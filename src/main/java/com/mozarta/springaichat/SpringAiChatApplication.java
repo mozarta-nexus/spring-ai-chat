b13832ac1e7cf6a5bf6d74a45e3d5c70b230b2be
mozarta-nexus <1447153224@qq.com>
2025-11-30 10:14:17 +0800

    init

---
+++ src/main/java/com/mozarta/springaichat/SpringAiChatApplication.java
@@ -0,0 +1,13 @@
+package com.mozarta.springaichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiChatApplication.class, args);
+    }
+
+}
